b8a3e77984fa72b2bfa23c713002156a00470a9c
Nygaaard <anny2301@student.miun.se>
2024-05-13 15:28:27 +0200

    Improved registration validation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,14 @@ Routes for registration and login
 By Andreas Nygård
 */
 
+//Requires
 const express = require("express");
 const router = express.Router();
+const sqlite3 = require("sqlite3").verbose();
+require("dotenv").config();
+
+//Connect to database
+const db = new sqlite3.Database(process.env.DATABASE);
 
 //Regex
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -15,16 +21,44 @@ router.post("/register", async (req, res) => {
     const { firstname, lastname, email, username, password } = req.body;
 
     //Validate input
-    if (!firstname || !lastname || !email || !username || !password) {
-      return res.status(400).json({ error: "Fill in all fields" });
+    //Firstname
+    if (!firstname) {
+      return res.status(400).json({
+        error: "Firstname is required",
+      });
     }
-    //Validate username
+    //Lastname
+    if (!lastname) {
+      return res.status(400).json({
+        error: "Lastname is required",
+      });
+    }
+    //Email
+    if (!email) {
+      return res.status(400).json({
+        error: "Email is required",
+      });
+    }
+    //Username
+    if (!username) {
+      return res.status(400).json({
+        error: "Username is required",
+      });
+    }
+    //Password
+    if (!password) {
+      return res.status(400).json({
+        error: "Password is required",
+      });
+    }
+
+    //Validate username length
     if (username.length < 5) {
       return res.status(400).json({
         error: "Invalid username - must be at least 5 characters long.",
       });
     }
-    //Validate password
+    //Validate password length
     if (password.length < 8) {
       return res.status(400).json({
         error: "Invalid password - must be at least 8 characters long",
@@ -35,8 +69,37 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ error: "Invalid email address" });
     }
 
-    //If correct - save user
-    res.status(201).json({ message: "User created" });
+    //Check if user already exists
+    const sqlCheckUsername = "SELECT * FROM users WHERE username = ?";
+    db.get(sqlCheckUsername, [username], (err, usernameRow) => {
+      if (err) {
+        return res.status(500).json({ error: "Server error" });
+      }
+      if (usernameRow) {
+        return res.status(400).json({ error: "Username already exists" });
+      }
+
+      //Check if email already exists
+      const sqlCheckEmail = "SELECT * FROM users WHERE email = ?";
+      db.get(sqlCheckEmail, [email], (err, emailRow) => {
+        if (err) {
+          return res.status(500).json({ error: "Server error" });
+        }
+        if (emailRow) {
+          return res.status(400).json({ error: "Email already exists" });
+        }
+
+        // If correct information - save user
+        const sql = `INSERT INTO users(firstname, lastname, email, username, password) VALUES(?, ?, ?, ?, ?)`;
+        db.run(sql, [firstname, lastname, email, username, password], (err) => {
+          if (err) {
+            return res.status(400).json({ message: "Error creating user" });
+          } else {
+            return res.status(201).json({ message: "User created" });
+          }
+        });
+      });
+    });
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
